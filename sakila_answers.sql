cdf10e85fb77e9728b2358b18dcd2a53544bb4d6
unknown <desairohan155@gmail.com>
2024-10-22 11:28:18 +0100

    before lunch

--- sakila_answers.sql
+++ sakila_answers.sql
@@ -50,23 +50,78 @@ having count(*) > 1
 order by count(*) desc;
 
 -- 13. Which actor has appeared in the most films?
-select actor.first_name, actor.last_name, count(*) from actor
-Inner join film, film_actor
-on film.film_id = film_actor.film_id, actor.actor_id = film_actor.actor_id 
-group by last_name
-having count(*) > 1
-order by count(*) desc;
+SELECT actor.first_name, actor.last_name, COUNT(*) AS film_count
+FROM actor
+INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+GROUP BY actor.actor_id
+ORDER BY film_count DESC
+limit 1;
 
 -- 14. When is 'Academy Dinosaur' due?
+
+select release_year from film
+where film.title = 'Academy Dinosaur';
+
 -- 15. What is the average runtime of all films?
+select avg(length) as avergae_length
+from film;
+
 -- 16. List the average runtime for every film category.
+
+select avg(length) as avergae_length
+from film
+inner join film_category
+on film.film_id = film_category.film_id
+inner join category
+on category.category_id = film_category.category_id
+group by category.name;
+
 -- 17. List all movies featuring a robot.
+
+select title from film
+where special_features like 'robot';
+
 -- 18. How many movies were released in 2010?
+
+select count(*)
+from film
+where release_year = 2010;
+
 -- 19. Find the titles of all the horror movies.
+
+select title
+from film
+inner join film_category
+on film.film_id = film_category.film_id
+inner join category
+on film_category.category_id = category.category_id
+where category.name like '%horror%';
+
 -- 20. List the full name of the staff member with the ID of 2.
+
+select first_name, last_name from staff
+where staff_id = 2;
+
 -- 21. List all the movies that Fred Costner has appeared in.
+
+select title from film
+inner join film_actor
+on film.film_id = film_actor.film_id
+inner join actor
+on actor.actor_id = film_actor.actor_id
+where actor.first_name = 'Fred' and actor.last_name = 'Costner';
+
+
 -- 22. How many distinct countries are there?
+
+select distinct count(*)
+from country;
+
 -- 23. List the name of every language in reverse-alphabetical order.
+
+select name from language
+order by name desc;
+
 -- 24. List the full names of every actor whose surname ends with '-son' in alphabetical order by their forename.
 -- 25. Which category contains the most films?
 
